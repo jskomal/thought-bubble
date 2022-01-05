e40013105efda18669e3f21fc0fa4557dee55dda
Zach Liibbe <zliibbe@gmail.com>
2022-01-05 16:54:37 -0700

    Fix spacing in layout with border-box and Add error message

--- main.js
+++ main.js
@@ -3,29 +3,19 @@ var inputTitle = document.querySelector('#input-title');
 var inputBody = document.querySelector('#input-body');
 
 var buttonSave = document.querySelector('.button-save');
+var errorMessage = document.querySelector('.error-message')
 
 //data
 var ideas = [];
 
 //eventListeners
 
-//separate the function, don't use alert, add a p tag to the dom?
-buttonSave.addEventListener('click', function clickSave(e) {
-  e.preventDefault();
-  if (inputTitle.value === '' && inputBody.value === '') {
-    alert('New ideas require a Title and a Body!');
-  } else if (inputTitle.value === '') {
-    alert('New ideas require a Title!');
-  } else if (inputBody.value == '') {
-    alert('New ideas require a Body!');
-  } else {
-    var inputIdea = new Idea(inputTitle.value, inputBody.value);
-    ideas.push(inputIdea);
-    inputTitle.value = '';
-    inputBody.value= '';
-  }
-});
 
+//separate the function, don't use alert, add a p tag to the dom?
+buttonSave.addEventListener('click', clickSave);
+
+
+buttonSave.addEventListener('mouseover', validateError)
 buttonSave.addEventListener('mouseover', validateInputs);
 buttonSave.addEventListener('mouseout', validateInputs);
 
@@ -34,4 +24,31 @@ function validateInputs() {
   if (inputTitle.value === '' || inputBody.value === '') {
     buttonSave.classList.toggle('button-save-invalid');
   }
+}
+
+function show(element) {
+  element.classList.remove('hidden');
+}
+
+function hide(element) {
+  element.classList.add('hidden');
+}
+
+function clickSave(e) {
+  e.preventDefault();
+  if (inputTitle.value === '' || inputBody.value === ''){
+    show(errorMessage);
+  } else {
+    var inputIdea = new Idea(inputTitle.value, inputBody.value);
+    ideas.push(inputIdea);
+    inputTitle.value = '';
+    inputBody.value= '';
+  }
+}
+
+function validateError() {
+  if(inputTitle.value && inputBody.value) {
+    hide(errorMessage);
+  }
+
 }
