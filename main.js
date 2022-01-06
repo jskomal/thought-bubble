8a97afc2e102587eb38da31566cdca83a61b1276
Nick Orlov <ialwaysreply@gmail.com>
2022-01-06 16:05:14 -0700

    Add showStarredIdeas functionality

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ var inputTitle = document.querySelector('#input-title');
 var inputBody = document.querySelector('#input-body');
 var buttonSave = document.querySelector('.button-save');
 var buttonDelete = document.querySelector('button-delete');
+var buttonShowStarred = document.querySelector('.button-show-starred-ideas');
 var errorMessage = document.querySelector('.error-message');
 var cardFlex = document.querySelector('.card-view');
-
 //data
 var ideas = [];
 
@@ -16,6 +16,7 @@ buttonSave.addEventListener('mouseover', validateInputsAdd);
 buttonSave.addEventListener('mouseout', validateInputsRemove);
 cardFlex.addEventListener('click', toDelete);
 cardFlex.addEventListener('click', toStar);
+buttonShowStarred.addEventListener('click', filterStarred);
 
 //functions
 function validateInputsAdd() {
@@ -104,3 +105,45 @@ function updateCardView() {
   }
   cardFlex.innerHTML = emptyHTML;
 }
+
+function filterStarred(e) {
+  e.preventDefault()
+  if (buttonShowStarred.innerText === 'Show All Ideas') {
+    console.log('hi');
+    updateCardView();
+    buttonShowStarred.innerText = 'Show Starred Ideas';
+  } else {
+      var starredIdeas = ideas.map(function checkForStarred(element) {
+      if (element.isStarred) {
+      return element
+    }
+  })
+      for (var i = 0; i < starredIdeas.length; i++) {
+      if (starredIdeas[i] === undefined) {
+        starredIdeas.splice(i, 1);
+      }
+    }
+      var emptyHTML = '';
+      for (var i = 0; i < starredIdeas.length; i++) {
+        emptyHTML += `<article class='card' id=${starredIdeas[i].id}>
+        <div class='card-top-bar' id=${starredIdeas[i].isStarred}>
+          <input type='image' src= ${
+            starredIdeas[i].isStarred ? 'assets/star-active.svg' : 'assets/star.svg'
+          } id='button-star' />
+          <input type='image' src='assets/delete.svg' id='button-delete'/>
+        </div>
+        <section class='card-text'>
+          <h1 class='card-title'>${starredIdeas[i].title}</h1>
+          <p class='card-body'>${starredIdeas[i].body}</p>
+        </section>
+        <div class='card-bottom-bar'>
+          <input type='image' src='assets/comment.svg' id='button-image-comment' />
+          <button id='button-comment'>Comment</button>
+        </div>
+      </article>`;
+    }
+        cardFlex.innerHTML = emptyHTML;
+
+        buttonShowStarred.innerText = 'Show All Ideas';
+    }
+  }
